6e14f81cc756a70a8010176bf3806f868f915223
VivekPatil1223 <71131757+VivekPatil1223@users.noreply.github.com>
2020-11-19 22:05:19 +0530

    Add files via upload

---
+++ class.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+class Lion
+{
+    public:
+    void info()
+    {
+        cout<<"\n lion is a wild animal";
+        
+    }
+};
+int main()
+{
+    Lion b;
+    b.info();
+    return 0;
+}
